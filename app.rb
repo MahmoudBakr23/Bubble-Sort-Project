55d1a0d0f3ddfa7963a2995373b6e60b37305bc0
unknown <nusretbutunay@gmail.com>
2020-07-07 18:38:09 +0300

    work on part 1 of the project

--- app.rb
+++ app.rb
@@ -1,21 +1,49 @@
 # frozen_string_literal: true
-
-bubble_sort = [4, 3, 78, 2, 0, 2]
-
-swap = true
-index = 0
-while swap
-  swap = false
-  while index < bubble_sort.length - 1
-    if bubble_sort[index] > bubble_sort[index + 1]
-      swap = true
-      sorted = bubble_sort[index]
-      bubble_sort[index] = bubble_sort[index + 1]
-      bubble_sort[index + 1] = sorted
-    end
-    index += 1
-  end
+=begin
+def bubble_sort(numbers)
+  swap = true
   index = 0
+  while swap
+    swap = false
+    while index < numbers.length - 1
+      if numbers[index] > numbers[index + 1]
+        swap = true
+        sorted = numbers[index]
+        numbers[index] = numbers[index + 1]
+        numbers[index + 1] = sorted
+      end
+      index += 1
+    end
+    index = 0
+  end
+  return numbers
 end
 
-puts bubble_sort
+numbers = [4, 3, 78, 2, 0, 2]
+puts bubble_sort(numbers)
+=end
+
+def bubble_sort_by(numbers)
+  swap = true
+  index = 0
+  while swap
+    swap = false
+    while index < numbers.length - 1
+      next unless (yield numbers[index], numbers[index + 1]).positive?
+      swap = true
+      sorted = numbers[index]
+      numbers[index] = numbers[index + 1]
+      numbers[index + 1] = sorted
+      index += 1
+    end
+    index = 0
+  end
+  return numbers
+end
+
+numbers = [4,2,3,7,6]
+bsort_by = bubble_sort_by(numbers) do |left, right|
+  left - right
+end
+
+puts bsort_by
