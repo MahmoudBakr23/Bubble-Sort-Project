e76c2a10b67e50b257c43eb64d11e3e243f07cc8
Mahmoud Bakr <mbakr6821@gmail.com>
2020-07-07 17:04:08 +0200

    bubble_sort first part

--- app.rb
+++ app.rb
@@ -1,1 +1,21 @@
+# frozen_string_literal: true
 
+bubble_sort = [4, 3, 78, 2, 0, 2]
+
+swap = true
+index = 0
+while swap
+  swap = false
+  while index < bubble_sort.length - 1
+    if bubble_sort[index] > bubble_sort[index + 1]
+      swap = true
+      sorted = bubble_sort[index]
+      bubble_sort[index] = bubble_sort[index + 1]
+      bubble_sort[index + 1] = sorted
+    end
+    index += 1
+  end
+  index = 0
+end
+
+puts bubble_sort
